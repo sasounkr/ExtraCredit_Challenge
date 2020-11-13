c15eae48a259bfec8739ce8f3c7ef7f56eea64ca
Sasoun Krikorian <sasounkr@usc.edu>
2020-11-13 12:41:25 -0800

    had the wrong clang-format setting, so had to apply correct setting

--- ec/bt-postfix/expression.h
+++ ec/bt-postfix/expression.h
@@ -8,18 +8,16 @@
 using namespace std;
 
 struct Node {
-  char token;
-  shared_ptr<Node> parent;
-  shared_ptr<Node> left;
-  shared_ptr<Node> right;
-  Node(char x, shared_ptr<Node> left, shared_ptr<Node> right,
-       shared_ptr<Node> parent)
-      : token(x), left(left), right(right), parent(parent) {}
+    char token;
+    shared_ptr<Node> parent;
+    shared_ptr<Node> left;
+    shared_ptr<Node> right;
+    Node(char x, shared_ptr<Node> left, shared_ptr<Node> right, shared_ptr<Node> parent)
+            : token(x), left(left), right(right), parent(parent) {}
 };
 
 // assume all operands are left associative
-const map<char, int> OP_PRECEDENCE{
-    {'+', 0}, {'-', 0}, {'*', 1}, {'/', 1}, {'^', 2}};
+const map<char, int> OP_PRECEDENCE{{'+', 0}, {'-', 0}, {'*', 1}, {'/', 1}, {'^', 2}};
 bool greater_precendence(char lhs, char rhs);
 bool is_op(char t);
 
@@ -29,7 +27,7 @@ shared_ptr<Node> insert_op(shared_ptr<Node> root, char op);
 shared_ptr<Node> insert_num(shared_ptr<Node> root, char num);
 shared_ptr<Node> convert_to_tree(string expression);
 int evaluate(string expression);
-void evaluate_helper(shared_ptr<Node> root, stack<int> &tokens);
-void clear(shared_ptr<Node> &root);
+void evaluate_helper(shared_ptr<Node> root, stack<int>& tokens);
+void clear(shared_ptr<Node>& root);
 
 #endif
--- ec/bt-postfix/main.cpp
+++ ec/bt-postfix/main.cpp
@@ -4,23 +4,23 @@
 using namespace std;
 
 void print_inorder(shared_ptr<Node> root) {
-  if (!root)
-    return;
-  print_inorder(root->left);
-  cout << root->token;
-  print_inorder(root->right);
+    if (!root)
+        return;
+    print_inorder(root->left);
+    cout << root->token;
+    print_inorder(root->right);
 }
 
 int main() {
-  string input;
-  cin >> input;
-  auto root = convert_to_tree(input);
-  // this should print the expression in order
-  print_inorder(root);
-  cout << endl;
-  // once you have completed part 1, uncomment the line below
-  // cout << evaluate(input) << endl;
-  // deallocate memory
-  clear(root);
-  return 0;
+    string input;
+    cin >> input;
+    auto root = convert_to_tree(input);
+    // this should print the expression in order
+    print_inorder(root);
+    cout << endl;
+    // once you have completed part 1, uncomment the line below
+    // cout << evaluate(input) << endl;
+    // deallocate memory
+    clear(root);
+    return 0;
 }
